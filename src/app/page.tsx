59a82387318c436fdf46d5f69423350dd822b61c
Artur Sgibnev <55087938+Artur-Sg@users.noreply.github.com>
2023-11-26 12:50:37 +0400

    Fix mobile for welcome section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,17 +19,20 @@ export default function Page() {
     <ThemeProvider>
       <Header />
       <main className='main'>
-        <section className='flex flex-col md:flex-row items-center justify-center py-36 pt-44'>
+        <section className='flex flex-col lg:flex-row items-center justify-center py-36 pb-8 lg:py-36 lg:pt-44'>
           <div className='mb-6 md:mb-0'>
-            <div id='head' className='wow animate__animated animate__pulse animate__infinite mx-10'>
+            <div
+              id='head'
+              className='wow animate__animated animate__pulse animate__infinite mx-auto'
+            >
               <HeadSection />
             </div>
           </div>
-          <div className='flex-3 select-none lg:flex-1 lg:max-w-[30%] wow animate__animated animate__fadeInRight'>
-            <h1 className='text-3xl font-normal mb-3 text-gray-900 dark:text-slate-300'>
-              Welcome to{' '}
+          <div className='flex-3 select-none lg:flex-1 lg:max-w-[40%] wow animate__animated animate__fadeInRight px-5 md:px-10 text-center md:text-left'>
+            <h1 className='text-3xl font-normal mb-3 text-gray-900 dark:text-slate-300 pt-4'>
+              Welcome to
               <span className="font-['KdamThmorPro'] tracking-widest text-gray-800 dark:text-slate-300 rounded-md font-semibold">
-                Impulse
+                {' '}Impulse
               </span>
               : Pioneering Web3 Innovations with Expertise and Passion
             </h1>
@@ -37,7 +40,7 @@ export default function Page() {
               Discover Impulse: Your Gateway to Advanced Web3 Solutions. Our
               team of IT experts is passionate about blockchain and crypto,
               offering top-tier node operation services. Led by our unique
-              nocturnal mascot, we&aposre dedicated to ensuring a secure and
+              nocturnal mascot, we&apos;re dedicated to ensuring a secure and
               decentralized future
             </p>
           </div>
